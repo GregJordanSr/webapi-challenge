431b83159954737190b62c41aa60fb9dbbc0e947
Greg Jordan <gregljordan@gmail.com>
2019-08-09 19:32:15 -0400

    Completed CRUD for chores

--- chores/choresdb.js
+++ chores/choresdb.js
@@ -1,5 +1,20 @@
  const chores = [
-   
+   {
+    id : 1,
+    description : "Micheal Jordan",
+	notes : "Play basketball",
+	assignedTO : 2,
+	completed : true
+   },
+
+   {
+    id : 2,
+    "description" : "Greg Jordan",
+	"notes" : "learn javascript",
+	"assignedTO" : 2,
+	"completed" : false
+   },
+
 ]
 
 module.exports = chores;
