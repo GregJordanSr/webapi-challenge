0b06e32adc9bd74a853742a8efd70848a4e44591
Greg Jordan <gregljordan@gmail.com>
2019-08-09 17:38:51 -0400

    added get and post for chores

--- chores/choreRouter.js
+++ chores/choreRouter.js
@@ -4,5 +4,32 @@ const peopledb = require("../people/peopledb");
 
 
 const router = express.Router();
+   
+choreId = 1;
+
+router.get("/", (req, res) => {
+    res.status(200).json(choresdb);
+})
+
+router.get("/:id", (req, res) => {
+ 
+})
+
+router.post("/", (req, res) => {
+    const chore = req.body;
+     chore.id = choreId;
+     choreId = choreId + 1;
+    choresdb.push(chore);
+    res.status(201).json(choresdb);
+})
+
+router.put("/", (req, res) => {
+    
+})
+
+router.delete("/:id", (req, res) => {
+    
+})
+
 
 module.exports = router;
--- chores/choresdb.js
+++ chores/choresdb.js
@@ -1,34 +1,5 @@
  const chores = [
-    {
-        id: 0,
-        description:"",
-        notes: "",
-        assignedTo: 0,
-        completed: false,
-        
-    },
-    {
-        id: 1,
-        description:"",
-        notes: "",
-        assignedTo: 2,
-        completed: false,
-       
-    },
-    {
-        id: 2,
-        description:"",
-        notes: "",
-        assignedTo: 1,
-        completed: false,
-       
-    },
-    {
-        id: 3,
-        description:"",
-        notes: "",
-        assignedTo: 3,
-        completed: false,
-        
-    }
+   
 ]
+
+module.exports = chores;
